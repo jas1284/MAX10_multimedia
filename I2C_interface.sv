f9b523cbb47e1d14f32b985624cefd743910564d
Benjamin Gao <benjamingao99@gmail.com>
2023-05-05 13:13:58 -0500

    Add Comments to I2C_interface
    No functional code changes made

--- I2C_interface.sv
+++ I2C_interface.sv
@@ -1,3 +1,7 @@
+// Module for providing I2C output for configuring the registers on the SGTL5000 chip. 
+// This module is provided with a 50MHz clock, a destination register and data to send, and an enable 
+// It provides the output serial I2C lines and an acknowledge when it is ready to recieve the next write command
+
 module I2C_interface (
 	input  logic clk50,
 	input  logic reset_interface,
@@ -63,14 +67,14 @@ module I2C_interface (
 			scl_state <= 1;
 		end
 		else if(i2c_scl == 0 && scl_state == 1 && state == low && clk50_count == scl_low / 2) begin //rising scl edge and writing a bit
-			bit_addr <= bit_addr + 1;
+			bit_addr <= bit_addr + 1; //update bit counter and edge detection for sclock
 			scl_state <= 0;
 		end
 		if(sda_in == 1 && (state == ack_high || state == ack_low)) begin //recieved ack signal from slave
-			acknowledge_recieved <= 1;
+			acknowledge_recieved <= 1; //acknowledge signal for changing states
 		end
 		else if(state == stop) begin
-			acknowledge_recieved <= 0;
+			acknowledge_recieved <= 0; //reset acknowledge signal for next byte cycle
 			bit_addr <= 0;
 		end
 		
@@ -107,26 +111,26 @@ module I2C_interface (
 				end
 			end
 			delay_0 : begin
-				if(clk50_count >= 120000) begin
+				if(clk50_count >= 120000) begin //initial delay for SGTL5000 hardware to initialize. Only triggers on startup and reset.
 					nextstate = init_high;
 				end
 			end
 			init_high : begin
-				if(clk50_count >= scl_high) begin
+				if(clk50_count >= scl_high) begin //initial high SCL during start condition
 					nextstate = init_low;
 				end
 			end
 			init_low : begin
-				if(clk50_count >= scl_low) begin
+				if(clk50_count >= scl_low) begin //initial low SCL during start condition
 					nextstate = high;
 				end
 			end
 			high : begin
-				if(clk50_count >= scl_high) begin
+				if(clk50_count >= scl_high) begin //high SCL during bit write
 					nextstate = low;
 				end
 			end
-			low : begin
+			low : begin //low SCL during bit write: transitions into acknowledge bit if finished writing byte.
 				if(clk50_count >= scl_low) begin
 					if(bit_addr == 8 || bit_addr == 16 || bit_addr == 24 || bit_addr == 32 || bit_addr == 40) begin
 						nextstate = ack_high;
@@ -137,19 +141,19 @@ module I2C_interface (
 					
 				end
 			end
-			ack_high : begin
+			ack_high : begin //high SCL waiting for acknowledge
 				if(clk50_count >= scl_high) begin
 					nextstate = ack_low;
 				end
 			end
-			ack_low : begin
+			ack_low : begin //low SCL waiting for acknowledge
 				if(clk50_count >= scl_low) begin
 					if(acknowledge_recieved == 1 || acknowledge_recieved == 0) begin //TODO remove for testing, needs to check for ack
 						if(bit_addr == 40) begin
-							nextstate = stop;
+							nextstate = stop; //finished writing bytes, end condition
 						end
 						else begin
-							nextstate = high;
+							nextstate = high; //write next byte
 						end
 					end
 					else begin
@@ -157,12 +161,12 @@ module I2C_interface (
 					end
 				end
 			end
-			stop : begin
+			stop : begin //low SDA on STOP condition
 				if(clk50_count >= scl_high) begin
 					nextstate = delay;
 				end
 			end
-			delay : begin
+			delay : begin //delay between I2C write cycles
 				if(clk50_count >= scl_period) begin
 					nextstate = reset;
 				end
@@ -172,11 +176,13 @@ module I2C_interface (
 			
 		i2c_scl = 1'b1;
 		i2c_sda = 1'b1;
+		//scl, sda held high by default
 		case (state)
 			reset : begin
 				i2c_scl = 1'b1;
 				i2c_sda = 1'b1;
 			end
+			//start condition means pulling SDA low first then starting the SClock
 			init_high : begin
 				i2c_scl = 1'b1;
 				i2c_sda = 1'b0;
@@ -189,7 +195,7 @@ module I2C_interface (
 				i2c_scl = 1'b1;
 				i2c_sda = writedata[39 - bit_addr];
 			end
-			low : begin
+			low : begin //Needing the SDA data to be changed in the middle of the SCL cycle rather than on a rising or falling edge means if statements are nessecary to avoid further states.
 				i2c_scl = 1'b0;
 				i2c_sda = writedata[39 - bit_addr];
 				if(clk50_count > scl_low / 2) begin
@@ -199,11 +205,11 @@ module I2C_interface (
 					end
 				end
 			end
-			ack_high : begin
+			ack_high : begin //during acknowledge both SCL and SDA are released and the slave pulls them low through the tri-state buffer
 				i2c_scl = 1'b1;
 				i2c_sda = 1'b1;
 			end
-			ack_low : begin
+			ack_low : begin //after acknowledge is recieved begin writing the next bit unless there is a stop condition
 				i2c_scl = 1'b0;
 				if(clk50_count <= scl_low / 2) begin
 					i2c_sda = 1'b1;
